7e1e45f18b1a4c4786637f0f24f6e62e309b8ebf
Lahiru <lahiruamarathunga526@gmail.com>
2023-04-06 09:43:30 +0530

    remove unwanted files

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import { Bars } from "react-loader-spinner";
-
 export default function Loading() {
-  return (
-    <Bars
-      height="80"
-      width="80"
-      color="#4fa94d"
-      ariaLabel="bars-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
-  );
+  return <div>Loading...</div>;
 }
